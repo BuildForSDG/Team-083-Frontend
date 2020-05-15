4997dd32cb0e983357edc0543d781a9aec39f12b
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-05-15 07:50:35 +0100

    move to auth branch

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 // react plugin for creating charts
-import ChartistGraph from 'react-chartist';
 // @material-ui/core
 import { makeStyles } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
@@ -10,29 +9,15 @@ import Warning from '@material-ui/icons/Warning';
 import DateRange from '@material-ui/icons/DateRange';
 import LocalOffer from '@material-ui/icons/LocalOffer';
 import Update from '@material-ui/icons/Update';
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import AccessTime from '@material-ui/icons/AccessTime';
 import Accessibility from '@material-ui/icons/Accessibility';
-import BugReport from '@material-ui/icons/BugReport';
-import Code from '@material-ui/icons/Code';
-import Cloud from '@material-ui/icons/Cloud';
 // core components
 import GridItem from 'components/Grid/GridItem.js';
 import GridContainer from 'components/Grid/GridContainer.js';
-import Table from 'components/Table/Table.js';
-import Tasks from 'components/Tasks/Tasks.js';
-import CustomTabs from 'components/CustomTabs/CustomTabs.js';
 import Danger from 'components/Typography/Danger.js';
 import Card from 'components/Card/Card.js';
 import CardHeader from 'components/Card/CardHeader.js';
 import CardIcon from 'components/Card/CardIcon.js';
-import CardBody from 'components/Card/CardBody.js';
 import CardFooter from 'components/Card/CardFooter.js';
-
-import { bugs, website, server } from 'variables/general.js';
-
-import { dailySalesChart, emailsSubscriptionChart, completedTasksChart } from 'variables/charts.js';
-
 import styles from 'assets/jss/material-dashboard-react/views/dashboardStyle.js';
 
 const useStyles = makeStyles(styles);
@@ -48,10 +33,8 @@ export default function Dashboard() {
               <CardIcon color="warning">
                 <Icon>content_copy</Icon>
               </CardIcon>
-              <p className={classes.cardCategory}>Used Space</p>
-              <h3 className={classes.cardTitle}>
-                49/50 <small>GB</small>
-              </h3>
+              <p className={classes.cardCategory}>Current Balance</p>
+              <h3 className={classes.cardTitle}>N10,000</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
@@ -59,7 +42,7 @@ export default function Dashboard() {
                   <Warning />
                 </Danger>
                 <a href="#pablo" onClick={e => e.preventDefault()}>
-                  Get more space
+                  request funds
                 </a>
               </div>
             </CardFooter>
@@ -71,13 +54,13 @@ export default function Dashboard() {
               <CardIcon color="success">
                 <Store />
               </CardIcon>
-              <p className={classes.cardCategory}>Revenue</p>
-              <h3 className={classes.cardTitle}>$34,245</h3>
+              <p className={classes.cardCategory}>Expenses</p>
+              <h3 className={classes.cardTitle}>N34,245</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
                 <DateRange />
-                Last 24 Hours
+                This month
               </div>
             </CardFooter>
           </Card>
@@ -88,13 +71,13 @@ export default function Dashboard() {
               <CardIcon color="danger">
                 <Icon>info_outline</Icon>
               </CardIcon>
-              <p className={classes.cardCategory}>Fixed Issues</p>
-              <h3 className={classes.cardTitle}>75</h3>
+              <p className={classes.cardCategory}>Income</p>
+              <h3 className={classes.cardTitle}>N10,000</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
                 <LocalOffer />
-                Tracked from Github
+                This month
               </div>
             </CardFooter>
           </Card>
@@ -105,18 +88,39 @@ export default function Dashboard() {
               <CardIcon color="info">
                 <Accessibility />
               </CardIcon>
-              <p className={classes.cardCategory}>Followers</p>
-              <h3 className={classes.cardTitle}>+245</h3>
+              <p className={classes.cardCategory}>Profit</p>
+              <h3 className={classes.cardTitle}>N600,000</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
                 <Update />
-                Just Updated
+                This month
               </div>
             </CardFooter>
           </Card>
         </GridItem>
       </GridContainer>
+    </div>
+  );
+}
+
+/*
+
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import AccessTime from '@material-ui/icons/AccessTime';
+import BugReport from '@material-ui/icons/BugReport';
+import Code from '@material-ui/icons/Code';
+import Cloud from '@material-ui/icons/Cloud';
+import Table from 'components/Table/Table.js';
+import Tasks from 'components/Tasks/Tasks.js';
+import CustomTabs from 'components/CustomTabs/CustomTabs.js';
+import CardBody from 'components/Card/CardBody.js';
+import ChartistGraph from 'react-chartist';
+
+import { bugs, website, server } from 'variables/general.js';
+
+import { dailySalesChart, emailsSubscriptionChart, completedTasksChart } from 'variables/charts.js';
+
       <GridContainer>
         <GridItem xs={12} sm={12} md={4}>
           <Card chart>
@@ -236,6 +240,4 @@ export default function Dashboard() {
           </Card>
         </GridItem>
       </GridContainer>
-    </div>
-  );
-}
+*/
